6e3bbfc4b6e42206625b5920be0a41dee1ae0617
tacocat <carterbailey@protonmail.com>
2022-11-28 20:18:03 -0700

    fixing up some typos

--- openmp.hpp
+++ openmp.hpp
@@ -3,9 +3,6 @@
 
 #include <omp.h>
 
-// Unordered_map is parallel safe to read from but not parallel safe to write too.
-// This means that I need to put a mutex when I write to the hash
-
 /*
  * @brief An OPENMP version of the KMeans algorithm
  *
@@ -13,28 +10,24 @@
  * @param centroids - the centroids that we are using
  * @param clusteredSongs - An array of vectors to contain the final clustered songs
  * @param threadCount - the number of threads to use for parallelizing
- *
- *
  */
 void openMPKMean(std::vector<song> data, std::vector<song> centroids, std::vector<song>* clusteredSongs, int threadCount)
 {
-	// unordered map has o(n) operations instead of maps o(logn) operations
-	// we don't need the map to be ordered so we'll take the speedup
 	long unsigned int j;
 	int centroid;
-#pragma omp parallel num_threads(threadCount) default(none) private(j, centroid) shared(clusteredSongs, data, centroids)
+  #pragma omp parallel num_threads(threadCount) default(none) private(j, centroid) shared(clusteredSongs, data, centroids)
 
 	for (int i = 0; i < ROUNDS; i++)
 	{
-#pragma omp for
+  #pragma omp for
 		for (j = 0; j < data.size(); j++)
 		{
 			centroid = findClosestCentroid(data[j], centroids);
-#pragma omp critical
+  #pragma omp critical
 			clusteredSongs[centroid].push_back(data[j]);
 		}
 
-#pragma omp for
+  #pragma omp for
 		for (j = 0; j < centroids.size(); j++)
 		{
 			updateCentroid(centroids[j], clusteredSongs[j]);
