bc8cdf1dbe77354aaea0a95ef6a8556eb9e7dd0a
Carter Bailey <carterbailey@protonmail.com>
2022-11-30 18:49:33 -0700

    updating it to where serial and openmp make their own executable

--- serial.cpp
+++ serial.cpp
@@ -1,5 +1,4 @@
-#include "kmeans.hpp"
-#include "openmp.hpp"
+#include "lib/kmeans.hpp"
 
 #include <chrono>
 
@@ -30,18 +29,5 @@ int main(int argc, char** argv)
 	std::cout << "Writing the results out of the serial K Means algorithm\n";
 	writeToCSV(clusteredSongs, centroids, "serialResults.csv");
 
-	// OPENMP
-	data = getCSV();
-	std::vector<song> clusteredSongsMP[centroidCount];
-	centroids = generateCentroids(std::stoi(argv[1]), data);
-	std::cout << "Running the OpenMP K Means algorithm\n";
-	auto startMP = std::chrono::high_resolution_clock::now();
-	openMPKMean(data, centroids, clusteredSongsMP, 12);
-	auto stopMP = std::chrono::high_resolution_clock::now();
-
-	time_taken = std::chrono::duration_cast<std::chrono::milliseconds>(stopMP - startMP);
-	std::cout << "Time for OpenMP K Means is " << time_taken.count() << " ms\n";
-	std::cout << "Writing the results out of the serial K Means algorithm\n";
-	writeToCSV(clusteredSongsMP, centroids, "OpenMPResults.csv");
 	return 0;
 }
