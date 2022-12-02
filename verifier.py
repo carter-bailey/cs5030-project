929b996ffae79bf0f184c4e4567b58e67a03bcd8
Carter Bailey <carterbailey@protonmail.com>
2022-12-01 19:30:26 -0700

    adding verifier.py

---
+++ verifier.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+def verifier(original, new):
+    original.sort_values(["centroid", "energy"],axis = 0, ascending = True,inplace = True)
+    new.sort_values(["centroid", "energy"],axis = 0, ascending = True,inplace = True)
+    return original.equals(new)
+
+serial = pd.read_csv("serialResults.csv")
+mp = pd.read_csv("OpenMPResults.csv")
+#mpi = pd.read_csv("mpiResults.csv")
+
+print(f"openmp is same as serial? {verifier(serial, mp)}")
+#print(f"openmpi is same as serial? {verifier(serial, mpi)}")
