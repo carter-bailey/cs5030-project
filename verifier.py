037e4f67b86cb861bb2d7c8af78287de3a67c233
Carter Bailey <carterbailey@protonmail.com>
2022-12-01 23:44:19 -0700

    adding writing the sorted csv's to a file

--- verifier.py
+++ verifier.py
@@ -9,7 +9,17 @@ def verifier(original, new):
 
 serial = pd.read_csv("serialResults.csv")
 mp = pd.read_csv("OpenMPResults.csv")
-#mpi = pd.read_csv("mpiResults.csv")
+mpi = pd.read_csv("mpiResults.csv")
 
+serial.sort_values(["centroid", "energy"],axis = 0, ascending = True,inplace = True)
+serial.reset_index(drop=True,inplace=True)
+mp.sort_values(["centroid", "energy"],axis = 0, ascending = True,inplace = True)
+mp.reset_index(drop=True,inplace=True)
+mpi.sort_values(["centroid", "energy"],axis = 0, ascending = True,inplace = True)
+mpi.reset_index(drop=True,inplace=True)
+
+serial.to_csv("serialSorted.csv",index=False)
+mp.to_csv("mpSorted.csv",index=False)
+mpi.to_csv("mpiSorted.csv",index=False)
 print(f"openmp is same as serial? {verifier(serial, mp)}")
-#print(f"openmpi is same as serial? {verifier(serial, mpi)}")
+print(f"openmpi is same as serial? {verifier(serial, mpi)}")
